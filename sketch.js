4e3e89465ad82c52188249298a1dc4c2513b2433
yuibun <yuuibun@gmail.com>
2021-03-03 22:09:42 -0800

    draw canvas and ellipse to screen

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 function setup() {
-  // put setup code here
+	createCanvas(displayWidth, displayHeight);
 }
 
+// draw shape to screen
 function draw() {
-  // put drawing code here
+	background(46,33,67);
+	fill(253,55,119);
+	ellipse(50,50,80,80);
 }
